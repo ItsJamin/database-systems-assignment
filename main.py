c4f8fe5a00b74c36b7b6c374553379d4ce98ae0b
ItsJamin <jamonjamin4@gmail.com>
2024-06-26 15:18:48 +0200

    Added 'Fetching Data and formatting into Pandas Dataframe'

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import json
 import plotly.express as px
 import pandas as pd
 
-connection = psycopg2.connect(host="localhost", 
-                        dbname="bevoelkerungstudierende", 
-                        user="postgres", 
-                        password="1234", 
-                        port=5432)
+#---------------------Connection zur Database und Daten abholen---------------------#
+
+db_params = {
+    'dbname': 'bevoelkerungstudierende',
+    'user': 'postgres',
+    'password': '1234',
+    'host': 'localhost',
+    'port': 5432
+}
+
+connection = psycopg2.connect(**db_params)
 
 cursor = connection.cursor()
 
@@ -42,14 +48,18 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS hochschulen (
                );
                """)
 
+
 query = """
-SELECT b.bundesland, b.insgesamt, h.studierende, 
-       (h.studierende::FLOAT / b.insgesamt) * 100 AS prozentsatz
-FROM bevoelkerung b
-JOIN hochschulen h ON b.bundesland = h.bundesland;
+SELECT hochschulen.bundesland, SUM(hochschulen.anzahlStudierende) AS gesamtstudierende, SUM(bevoelkerung.insgesamt) AS gesamteinwohner
+FROM hochschulen, bevoelkerung
+WHERE hochschulen.bundesland = bevoelkerung.bundesland
+GROUP BY hochschulen.bundesland
 """
 
-#df = pd.read_sql_query(query, connection)
+cursor.execute(query)
+
+rows = cursor.fetchall()
+col_names = [desc[0] for desc in cursor.description]
 
 
 # connection beenden
@@ -57,6 +67,20 @@ connection.commit()
 cursor.close()
 connection.close()
 
+
+
+#---------------------Auf dem Dataframe arbeiten---------------------#
+
+df = pd.DataFrame(rows, columns=col_names)
+
+df['percentage'] = (df['gesamtstudierende'] / df['gesamteinwohner']) * 100
+
+# Format the percentage
+df['percentage'] = df['percentage'].map(lambda x: f'{x:.2f}%')
+
+# Display the desired output
+result = df[['bundesland', 'percentage']]
+print(result)
 
 # heatmap erstellen
 #import plotly.express as px
@@ -67,10 +91,3 @@ connection.close()
 #                         color_continuous_scale=px.colors.sequential.Viridis)
 
 #fig.show()
-
-"""
-SELECT hochschulen.bundesland, SUM(hochschulen.studierendenanzahl) AS gesamtstudierende, bevoelkerung.insgesamt
-FROM hochschulen, bevoelkerung
-WHERE hochschulen.bundesland = bevoelkerung.bundesland
-GROUP BY bundesland
-"""
